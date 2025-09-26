f9ebc11ddf21f3cd5164883cca5fda576cdc6c28
Webmaster <matt@homexa.app>
2025-09-26 13:10:36 -0400

    Target only first-party iframes with iframeResizer

--- init_iframe_resize.js
+++ init_iframe_resize.js
@@ -1,3 +1,7 @@
+const resizable_iframes = Array.from(document.querySelectorAll('iframe')).filter((iframe) => {
+    return iframe.src.startsWith(window.location.origin);
+});
+
 iframeResize({
     license: 'GPLv3',
-}, 'iframe');
+}, ...resizable_iframes);
